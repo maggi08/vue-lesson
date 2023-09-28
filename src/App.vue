c5541fec06c9eab10eea5543b5e02a4bdb10aa6f
Mako <maggi08mus@gmail.com>
2023-09-28 21:31:21 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    <h1 v-on:click="hello">Hello Vue</h1>
+
+    <CounterComp />
+  </div>
+</template>
+
+<script>
+import CounterComp from '@/components/CounterComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CounterComp
+  },
+  mounted() {
+    // oneFunction()
+  },
+  methods: {
+    hello() {
+      alert('hello')
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
---
+++ src/components/CounterComp.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="">
+    <h1>Count: {{ count }}</h1>
+
+    <button @click="inc">Click to Count</button>
+    <button @click="dec">Click to Decriment</button>
+  </div>
+</template>
+
+<!-- <script setup>
+import { ref } from 'vue'
+
+let count = ref(0)
+
+function inc() {
+  count.value = count.value + 1
+}
+</script> -->
+
+<script>
+export default {
+  name: 'CounterComp',
+  data() {
+    return {
+      count: 10
+    }
+  },
+  created() {
+    console.log('Our component is created', this.count)
+  },
+  mounted() {
+    console.log('Our component is mounted', this.count)
+    this.inc()
+  },
+  methods: {
+    inc() {
+      this.count++
+      this.log()
+    },
+    dec() {
+      this.count--
+      this.log()
+    },
+    log() {
+      console.log('Counter now is - ', this.count)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: pink;
+}
+</style>
