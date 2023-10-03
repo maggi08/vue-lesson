c96ad1805c57cf94843da252f8e4450e4e90a208
Mako <maggi08mus@gmail.com>
2023-10-03 19:54:55 +0600

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,91 @@
 <template>
   <div class="">
-    <h1 v-on:click="hello">Hello Vue</h1>
+    <ListComp style="margin-bottom: 1000px" />
+
+    <button @click="toggle()">Toggle</button>
+    <button @click="isToggled2 = !isToggled2">Toggle 2</button>
+    <br />
+    <br />
+    <br />
+
+    <button
+      @click="isShown = false"
+      @mouseover="isHovered = true"
+      @mouseleave="isHovered = false"
+      :style="isHovered ? 'background: green' : ''"
+    >
+      hide
+    </button>
+
+    <button v-if="!isHovered" @click="isShown = true">show</button>
+
+    <div
+      v-if="isShown && isToggled"
+      class="div__default"
+      style="
+        background: black;
+        display: flex;
+        align-items: center;
+        color: white;
+        width: 200px;
+        height: 80px;
+      "
+      :class="{ div__toggled: isToggled }"
+      :style="[
+        isToggled ? 'background: green; color: yellow;' : 'background: purple; color: white;',
+        isToggled2 ? 'background: red;' : ''
+      ]"
+    >
+      <!-- isToggled ? 'background: green' : 'background: purple',
+        isToggled ? 'color: yellow' : 'color: white' -->
+      <!-- :style="`background: ${isToggled ? 'green' : 'black'}`" -->
+      isToggled {{ isToggled }}
+      <br />
+      isToggled2 {{ isToggled2 }}
+    </div>
+
+    <div
+      v-else-if="isShown && !isToggled"
+      style="
+        background: rgb(0, 30, 255);
+        display: flex;
+        align-items: center;
+        color: red;
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+      "
+    ></div>
+
+    <div
+      v-else
+      style="
+        background: rgb(0, 246, 226);
+        display: flex;
+        align-items: center;
+        color: red;
+        width: 200px;
+        height: 80px;
+      "
+    ></div>
+
+    <!-- <h1 @click="hello">Hello Vue</h1> -->
+
+    <!-- <h2>{{ firstName }}</h2>
+    <br />
+    <br />
+    <h2 v-html="lastName"></h2>
+    <br />
+    <br />
+    <h2>{{ lastName }}</h2>
+    <br />
+    <br />
+
+    <h1>
+      {{ 2 + 2 + lastName }}
+    </h1>
+
+    <div :id="oneDiv" class="">{{ calculateResult() }}</div> -->
 
     <CounterComp />
   </div>
@@ -8,18 +93,40 @@
 
 <script>
 import CounterComp from '@/components/CounterComp.vue'
+import ListComp from '@/components/ListComp.vue'
 
 export default {
   name: 'App',
   components: {
-    CounterComp
+    CounterComp,
+    ListComp
+  },
+  data() {
+    return {
+      firstName: 'My surename is <br/> Jhon',
+      lastName: 'My surename is <br/> Smith',
+      oneDiv: 'AnotherDiv',
+      isToggled: false,
+      isToggled2: false,
+      isShown: false,
+      isHovered: false
+    }
   },
   mounted() {
     // oneFunction()
+    // console.log(document.getElementById('oneDiv'))
+    // console.log(document.getElementById(this.oneDiv))
+    // console.log(document.getElementById('AnotherDiv'))
   },
   methods: {
     hello() {
       alert('hello')
+    },
+    calculateResult() {
+      return 2 + 5 + 123 + 92034
+    },
+    toggle() {
+      this.isToggled = !this.isToggled
     }
   }
 }
@@ -28,5 +135,15 @@ export default {
 <style>
 h1 {
   color: green;
+}
+
+.div__default {
+  border-radius: 10%;
+}
+button:hover {
+  background: black;
+}
+.div__toggled {
+  border-radius: 50%;
 }
 </style>
---
+++ src/components/ListComp.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="px-4">
+    <h1>List of Numbers</h1>
+
+    <p>Can you show me all colors from array</p>
+
+    <div class="" style="display: grid; grid-template-columns: 1fr 1fr">
+      <p
+        v-for="(color, index) in colors"
+        :key="index"
+        :style="`background: ${color}; color: white;`"
+        class=""
+      >
+        {{ color }} {{ index }}
+      </p>
+    </div>
+
+    <p>Can you show me numbers from 1 to 100</p>
+
+    <p v-for="num in 100" :key="num">{{ num }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: ['red', 'black', 'green', 'yellow', 'blue', 'purple', 'green', 'green', 'green']
+    }
+  }
+}
+</script>
+
+<style scoped>
+.px-4 {
+  padding: 0 16px;
+}
+</style>
